36acb6780803f6f3d1863fad77cfc05cfbed0d8a
gerardmv <gerardmvela@hotmail.com>
2020-03-14 13:07:09 +0100

    Se modifica la clase Principal, y se corrige el error del metodo factorialRecursiu.

--- src/operacions/Principal.java
+++ src/operacions/Principal.java
@@ -2,13 +2,21 @@ package operacions;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		Factorial fact = new Factorial();
-		try {
-			double valor = fact.factorialIteratiu(4);
-			System.out.println(valor);
-		} catch (IllegalArgumentException e) {
-			System.out.println("No es poden fer servir nombres negatius.");
-		}
+    public static void main(String[] args) {
+	Factorial fact = new Factorial();
+	try {
+	    double valor = fact.factorialIteratiu(4);
+	    System.out.println(valor);
+	} catch (IllegalArgumentException e) {
+	    System.out.println("No es poden fer servir nombres negatius.");
 	}
+
+	fact = new FactorialRecursiu();
+	try {
+	    double valor = fact.factorialRecursiu(3);
+	    System.out.println(valor);
+	} catch (IllegalArgumentException e) {
+	    System.out.println("No es poden fer servir nombres negatius.");
+	}
+    }
 }
